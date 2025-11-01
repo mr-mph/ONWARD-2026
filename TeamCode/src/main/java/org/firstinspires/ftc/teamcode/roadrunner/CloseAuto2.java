4531aa17cc180886bcbe57de14192be900c8dfe1
jack-the-bucket <skellys913@gmail.com>
2025-11-01 14:33:44 -0700

    Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/CloseAuto2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/CloseAuto2.java
@@ -1,13 +1,6 @@
 package org.firstinspires.ftc.teamcode.roadrunner;
 
-import com.acmerobotics.dashboard.config.Config;
-
-import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.SleepAction;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -15,44 +8,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
-import org.firstinspires.ftc.teamcode.roadrunner.PinpointLocalizer;
-import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
-
-@Config
 @Autonomous(name = "!! 3 Specimen Close Auto ", group = "! Auto")
 public class CloseAuto2 extends LinearOpMode
 
 {
 
     @Override
-    public void runOpMode()
+    public void runOpMode() throws InterruptedException
     {
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(new Vector2d(55.7010989010989, 6.804395604395612), 90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(new Vector2d(0, 0), 0));
 
         DcMotorEx intake = hardwareMap.get(DcMotorEx.class,"intake");
 
-        Pose2d startPose = new Pose2d(55.7010989010989,6.804395604395612, Math.toRadians(90));
-
-
         waitForStart();
 
-        TrajectoryActionBuilder line1 = drive.actionBuilder(startPose)
-                .strafeTo(new Vector2d(56, 36))
-                .endTrajectory();
-
-        TrajectoryActionBuilder line2 = line1.fresh()
-                .strafeTo(new Vector2d(14.716483516483516, 35.28791208791209))
-                .endTrajectory();
-
-
-        Actions.runBlocking(new SequentialAction(
-                line1.build(),
-                new InstantAction(() -> {intake.setPower(1);} ),
-                line2.build(),
-                new InstantAction(() -> {intake.setPower(0);} )
-        ));
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(0,0,0))
+                        .lineToX(50)
+                        .lineToX(0)
+                        .build());
     }
-
-
 }

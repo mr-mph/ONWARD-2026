670a979cda40fb0d7e7a589e135f2ffa7ca307cc
jack-the-bucket <skellys913@gmail.com>
2025-11-07 14:52:35 -0800

    Update CloseAuto2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/CloseAuto2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/CloseAuto2.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.roadrunner;
 
+import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
@@ -20,13 +24,37 @@ public class CloseAuto2 extends LinearOpMode
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(new Vector2d(0, 0), 0));
 
         DcMotorEx intake = hardwareMap.get(DcMotorEx.class,"intake");
+        DcMotorEx launcherLeft = hardwareMap.get(DcMotorEx.class, "launcher left");
+        DcMotorEx launcherRight = hardwareMap.get(DcMotorEx.class, "launcher right");
+        launcherLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        launcherRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        Pose2d startPos = new Pose2d(new Vector2d(0, 0), 0);
 
         waitForStart();
 
-        Actions.runBlocking(
-                drive.actionBuilder(new Pose2d(0,0,0))
-                        .lineToX(50)
-                        .lineToX(0)
-                        .build());
+        TrajectoryActionBuilder line1 = drive.actionBuilder(startPos)
+                .strafeTo(new Vector2d(50, 0))
+                .endTrajectory();
+        TrajectoryActionBuilder line2 = line1.fresh()
+                .strafeTo(new Vector2d(50, 0))
+                .endTrajectory();
+
+
+
+        Actions.runBlocking(new SequentialAction(
+                line1.build(),
+                new InstantAction(() -> {
+                    intake.setPower(1);
+                }),
+                line2.build(),
+                new InstantAction(() -> {
+                    intake.setPower(0);
+                }),
+                new InstantAction(() -> {
+                    launcherLeft.setPower(1);
+                    launcherRight.setPower(-1);
+                })
+        ));
     }
 }

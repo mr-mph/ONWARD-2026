ca3f869fa4027fecaf6c7acb85f11f9a5ad0fcb2
jack-the-bucket <skellys913@gmail.com>
2025-11-07 15:54:23 -0800

    Update QuinAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuinAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuinAuto.java
@@ -104,16 +104,60 @@ public class QuinAuto extends LinearOpMode {
 				.endTrajectory();
 		TrajectoryActionBuilder line2 = line1.fresh()
 				.strafeTo(new Vector2d(14.6, 36))
-				.turn(180)
 				.endTrajectory();
 		TrajectoryActionBuilder line3 = line2.fresh()
 				.strafeTo(new Vector2d(73.3, 63.3))
+				.turn(135)
+				.endTrajectory();
+		TrajectoryActionBuilder line4 = line3.fresh()
+				.strafeTo(new Vector2d(54, 100))
+				.endTrajectory();
+		TrajectoryActionBuilder line5 = line4.fresh()
+				.strafeTo(new Vector2d(53.7, 60))
+				.turn(180)
+				.endTrajectory();
+		TrajectoryActionBuilder line6 = line5.fresh()
+				.strafeTo(new Vector2d(12.6, 60))
+				.endTrajectory();
+		TrajectoryActionBuilder line7 = line6.fresh()
+				.strafeTo(new Vector2d(65.7, 77.7))
+				.turn(135)
+				.endTrajectory();
+		TrajectoryActionBuilder line8 = line7.fresh()
+				.strafeTo(new Vector2d(54, 100))
+				.endTrajectory();
+		TrajectoryActionBuilder line9 = line8.fresh()
+				.strafeTo(new Vector2d(54, 84))
+				.turn(180)
+				.endTrajectory();
+		TrajectoryActionBuilder line10 = line9.fresh()
+				.strafeTo(new Vector2d(11.8, 84))
+				.endTrajectory();
+		TrajectoryActionBuilder line11 = line10.fresh()
+				.strafeTo(new Vector2d(54, 100))
 				.turn(135)
 				.endTrajectory();
 
 		Actions.runBlocking(new SequentialAction(
 				line1.build(),
 				new InstantAction(() -> {intake.setPower(1);}),
+				line2.build(),
+				new InstantAction(() -> {intake.setPower(0);}),
+				line3.build(),
+				line4.build(),
+				launch2Artifacts
+				line5.build(),
+				new InstantAction(() -> {intake.setPower(1);}),
+				line6.build(),
+				new InstantAction(() -> {intake.setPower(0);}),
+				line7.build(),
+				line8.build(),
+				launch2Artifacts,
+				line9.build(),
+				new InstantAction(() -> {intake.setPower(1);}),
+				line10.build(),
+				new InstantAction(() -> {intake.setPower(0);}),
+				line11.build(),
 				launch2Artifacts
 		));
 	}
